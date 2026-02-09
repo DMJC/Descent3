6725e193cb6f66fa85c674a3b9adcb883418bcf8
DMJC <jcarthew@gmail.com>
2026-02-10 02:10:11 +1100

    Render HUD in VR stereo frames

--- Descent3/GameLoop.cpp
+++ Descent3/GameLoop.cpp
@@ -2489,6 +2489,9 @@ void GameRenderWorld(object *viewer, vector *viewer_eye, int viewer_roomnum, mat
     viewer->orient = save_orient;
 }
 
+// Added by Samir
+#define HUD_RENDER_ZOOM 0.56f
+
 // Render into the big window
 void GameDrawMainView() {
   extern bool Guided_missile_smallview; // smallviews.cpp
@@ -2530,6 +2533,15 @@ void GameDrawMainView() {
                       is_left_eye, VR_GetStereoEyeSeparation(), kVrConvergenceDistance);
       DoMatcensRenderFrame();
       ProcessRenderEvents();
+      if (!HUD_disabled) {
+        g3_StartFrame(&Viewer_object->pos, &view_orient, HUD_RENDER_ZOOM);
+        RenderHUDFrame();
+        g3_EndFrame();
+
+        g3_StartFrame(&Viewer_object->pos, &view_orient, HUD_RENDER_ZOOM);
+        RenderAuxHUDFrame();
+        g3_EndFrame();
+      }
       EndFrame();
       return rend_Screenshot();
     };
@@ -2560,9 +2572,6 @@ void GameDrawMainView() {
     Viewer_object = save_view;
 
 }
-
-// Added by Samir
-#define HUD_RENDER_ZOOM 0.56f
 
 // Do Cockpit/Hud
 void GameDrawHud() {
